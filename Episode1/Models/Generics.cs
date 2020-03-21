4a9eee020bb14e5bb76d27e53b0e56f5d0a77b52
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2020-03-21 20:15:36 +0100

    Episode 1, 2

---
+++ Episode1/Models/Generics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Episode1.Models
+{ 
+    public class Result<T>
+    {
+        public T Item { get; set }
+    }
+
+    public class GenericOrderProcessor
+    {
+        public Order ProcessOrder(string email, int orderId)
+        {
+            return new Order(1, 100);
+        }
+    }
+
+    public class Generics
+    {
+        public void Test()
+        {
+            List<User> user = new List<User>();
+        }
+    }
+}
