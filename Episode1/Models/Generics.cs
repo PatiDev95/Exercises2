8400931f1e361ab9cde6f00239155ecfef0cda9a
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2020-03-23 16:00:22 +0100

    Generics

--- Episode1/Models/Generics.cs
+++ Episode1/Models/Generics.cs
@@ -6,22 +6,63 @@ namespace Episode1.Models
 { 
     public class Result<T>
     {
-        public T Item { get; set }
+        public T Item { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public string Error { get; set; }
     }
 
+    public class Pair<TFirst, TSecond>
+    {
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+    }
+
+    public class Triple<TFirst, TSecond, TThird> : Pair<TFirst, TSecond>
+    {
+        public TThird Third { get; set; }
+    }
     public class GenericOrderProcessor
     {
-        public Order ProcessOrder(string email, int orderId)
+        public Result <Order> ProcessOrder(string email, int orderId)
         {
-            return new Order(1, 100);
+            Order order = new Order(1, 100);
+
+            return new Result<Order>
+            {
+                Item = order
+            };
+        }
+        public void LogOrder<TOrder, TSecondOrder>(TOrder order, TSecondOrder secondOrder) where TOrder : Order where TSecondOrder : Order
+        {
+
         }
     }
+
+    
 
     public class Generics
     {
         public void Test()
         {
-            List<User> user = new List<User>();
+            GenericOrderProcessor orderProcessor = new GenericOrderProcessor();
+            Result<Order> result = orderProcessor.ProcessOrder("email@email.com", 1);
+
+            Pair<int, string> pair = new Pair<int, string>();
+            Triple<int, string, bool> triple = new Triple<int, string, bool>();
+
+            ReferenceResult<User> intResult = new ReferenceResult<User>();
+            ValueResult<int> valueResult = new ValueResult<int>();
         }
+    }
+
+    public class ReferenceResult<T> where T : class
+    {
+        public T Result { get; set; }
+    }
+    public class ValueResult<T> where T : struct
+    {
+        public T Result { get; set; }
     }
 }
