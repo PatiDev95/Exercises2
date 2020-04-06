0c083b54f3d66b923b61741683617787b39974b8
PatiDev95 <kubinskapatrycja@gmail.com>
2020-04-06 23:29:30 +0200

    Integration tests, BDD

---
+++ Tests/WebTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class WebTests
+    {
+        public HttpClient HttpClient;
+
+        [SetUp]
+        public void Setup()
+        {
+            HttpClient = new HttpClient();
+
+        }
+
+        [Test]
+        public async Task http_response_should_have_204_status_code()
+        {
+            //Arrange
+
+            //Act
+            var response = await HttpClient.GetAsync("http://httpstat.us/204");
+
+            //Assert
+            response.Should().NotBeNull();
+            response.StatusCode.ShouldBeEquivalentTo(HttpStatusCode.NoContent);
+            //Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+
+        }
+    }
+}
